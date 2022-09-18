d7639bf535a4cb14a9001981be8e7d9b4cc4f7c4
HasanMasrur <mhbabu99@gmail.com>
2022-09-18 23:01:02 +0600

    file rename,and deleted

--- file.js
+++ file.js
@@ -7,8 +7,19 @@ fs.writeFile('file.txt','hello program , i am hasan ',function(err){
 if(err) throw err;
 console.log("saved")
 })
-
+// update
 fs.appendFile('file.txt','i am developer',function(err){
     if(err) throw err;
     console.log("saved");
+})
+
+// rename 
+fs.rename('file.txt', 'myfile.txt',function(err){
+    if(err) throw err ;
+    console.log("saved!");
+}),
+// deleted
+fs.unlink('myfile.txt',function(err){
+    if(err) throw err ;
+    console.log("saved !");
 })
