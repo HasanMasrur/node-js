b2974b27bc272c3774dccb988bdc2d2874cb4cd3
HasanMasrur <mhbabu99@gmail.com>
2022-09-18 22:54:22 +0600

    append file

--- file.js
+++ file.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
-fs.writeFileSync('file.txt','hello program')
-fs.appendFileSync('file.txt'," i am C")
-fs.appendFileSync('file.txt',". language")
+// fs.writeFileSync('file.txt','hello program')
+// fs.appendFileSync('file.txt'," i am C")
+// fs.appendFileSync('file.txt',". language")
+fs.writeFile('file.txt','hello program , i am hasan ',function(err){
+if(err) throw err;
+console.log("saved")
+})
+
+fs.appendFile('file.txt','i am developer',function(err){
+    if(err) throw err;
+    console.log("saved");
+})
