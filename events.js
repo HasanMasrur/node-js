eef6d07b9e3e8eeb1668637c387f8438458c6dc5
HasanMasrur <mhbabu99@gmail.com>
2022-09-23 11:47:51 +0600

    added http

--- events.js
+++
@@ -1,8 +0,0 @@
-
- const School = require('./school');
- const  school = new School();
-school.on('start',({bang, english})=>{
-  console.log(`i hear a scream ${bang} ${english}`);
-})
-school. startClasa();
-
